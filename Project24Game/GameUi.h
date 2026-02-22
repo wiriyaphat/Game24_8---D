e5b253f368561f52982ffcf2ba8135c25ebcedcb
Thiw <wiriyaphat6789@gmail.com>
2026-02-22 18:27:24 +0700

    Up to date 22/2

--- Project24Game/GameUi.h
+++ Project24Game/GameUi.h
@@ -3,7 +3,6 @@
 #include <string>
 using namespace std;
 
-float TittleGameSize = 100;
 string Fontloc = "C:/Windows/Fonts/arial.ttf";
 float windowSize_x = 1600;
 float WindowSize_y = 900;
@@ -16,15 +15,17 @@ class buttonBuild{
         float FontSize;
         float buttonSize_x;
         float buttonSize_y;
-        float shiftX;
-        float shiftY;
+        float X;
+        float Y;
         string name;
+        sf::Color ColorBox;
         sf::RectangleShape builtButton(); 
-        sf::Text txtBox();
+        sf::Text txtBox(sf::Vector2f);
 };
 
 class Title{
-    string Title_string = "--Game24--";
+    string Title_string = "Game24";
+    float TittleGameSize = 100;
     public:
         sf::Text TitleName();
 };
@@ -32,7 +33,7 @@ class Title{
 
 sf::Text Title::TitleName(){
     sf::Text Title_24(font,Title_string,TittleGameSize);
-    Title_24.setPosition({windowSize_x/2-TittleGameSize-50,50});
+    Title_24.setPosition({windowSize_x*(2.5/5)-TittleGameSize-50,50});
     return Title_24;
 };
 
@@ -40,15 +41,16 @@ sf::Text Title::TitleName(){
 
 sf::RectangleShape buttonBuild::builtButton(){
     sf::RectangleShape button({buttonSize_x,buttonSize_y}); //Create button variable with size
+    button.setOrigin(button.getGeometricCenter());
     button.setPosition({posBox_x,posBox_y}); 
-    button.setFillColor(sf::Color::Blue);
+    button.setFillColor(ColorBox);
     return button;
 };
 
-sf:: Text buttonBuild::txtBox(){
+sf:: Text buttonBuild::txtBox(sf::Vector2f buttonCenter){
     sf::Text Text(font,name,FontSize);
-    Text.setPosition({posBox_x+(buttonSize_x/2)+shiftX,posBox_y+(buttonSize_y/2)+shiftY});
+    Text.setOrigin(buttonCenter);
+    Text.setPosition({posBox_x+X,posBox_y+Y});
     return Text;
 }
-
 
