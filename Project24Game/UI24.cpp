894598d1ba18876e6fff1d6a747065afe463e5b6
Thiw <wiriyaphat6789@gmail.com>
2026-02-22 18:27:09 +0700

    Clear window ready for game

--- Project24Game/UI24.cpp
+++ Project24Game/UI24.cpp
@@ -10,31 +10,40 @@ int main()
 
     // button24
     buttonBuild button24 = {
-        .posBox_x = windowSize_x/6,
+        .posBox_x = windowSize_x*1/5,
         .posBox_y = WindowSize_y/(float)2, // 2.5 when no float was assian as double type
         .FontSize = 75,
-        .buttonSize_x = 225,
-        .buttonSize_y = 75,
-        .shiftX = -110,
-        .shiftY = -50,
-        .name = "--Game24--"
+        .buttonSize_x = 300,
+        .buttonSize_y = 100,
+        .X = 5,
+        .Y = 0,
+        .name = "Game24",
+        .ColorBox = sf::Color(255,0,0)
     };
 
     // buttonN
     buttonBuild buttonN = {
-        .posBox_x = windowSize_x*4/6,
+        .posBox_x = windowSize_x*4/5,
         .posBox_y = WindowSize_y/(float)2, // 2.5 when no float was assian as double type
         .FontSize = 75,
-        .buttonSize_x = 225,
-        .buttonSize_y = 75,
-        .shiftX = -110,
-        .shiftY = -50,
-        .name = "Random"
+        .buttonSize_x = 300,
+        .buttonSize_y = 100,
+        .X = 5,
+        .Y = 0,
+        .name = "Random",
+        .ColorBox = sf::Color(0,250,0)
     };
-
+    
     sf:: RectangleShape button_24 = button24.builtButton();
     sf:: RectangleShape button_N = buttonN.builtButton();
+
+    // for check mouse is over button or not and click button or not so we process more easily
+    bool isOver_24 = false;
+    bool isOver_N = false;
+    bool isClicked_24 = false;
+    bool isClicked_N = false;
     
+    // GameOn
     while (window.isOpen()) // using to make window always open 
     {
         while (const std::optional event = window.pollEvent()) // Closing windows functions
@@ -45,14 +54,18 @@ int main()
         if (button_24.getGlobalBounds().contains(mouse_pos)) // check button is coline with mouse : augmentNeedtoCheck.getGlobalBound().contains(whatIscheckwith); >> getGlobalBound() = check coline
         {
             button_24.setFillColor(sf::Color(0,75,22)); // we can create animated or click to process
+            isOver_24 = true;
         }else{
-            button_24.setFillColor(sf::Color::Blue); // reset went not set
+            button_24.setFillColor(button24.ColorBox); // reset went not set
+            isOver_24 = false;
         }
         if (button_N.getGlobalBounds().contains(mouse_pos)) // check button is coline with mouse : augmentNeedtoCheck.getGlobalBound().contains(whatIscheckwith); >> getGlobalBound() = check coline
         {
             button_N.setFillColor(sf::Color(0,75,22)); // we can create animated or click to process
+            isOver_N = true;
         }else{
-            button_N.setFillColor(sf::Color::Red); // reset went not set
+            button_N.setFillColor(buttonN.ColorBox); // reset went not set
+            isOver_N = false;
         }
 
         
@@ -61,9 +74,31 @@ int main()
         window.draw(tilte.TitleName());
         window.draw(button_24);
         window.draw(button_N);
-        window.draw(button24.txtBox());
-        window.draw(buttonN.txtBox());    
-        
+        window.draw(button24.txtBox(button24.builtButton().getGeometricCenter()));
+        window.draw(buttonN.txtBox(buttonN.builtButton().getGeometricCenter()));    
+
+        //------------------------------------------------------------------
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) // clicked 
+        {
+            if(isOver_24){ // clicked happen on button_24 or not 
+                isClicked_24 = true;
+            }
+
+            if(isOver_N){ // clicked happen on button_N or not
+                isClicked_N = true;
+            }
+            
+        }
+        if(isClicked_24) 
+        {
+            window.clear(sf::Color::Black); // Start GAME24
+        }
+
+        if(isClicked_N)
+        {
+            window.clear(sf::Color::Red); // Start RANDOM mode
+        }
+    
         // end the current frame
         window.display();
     }
